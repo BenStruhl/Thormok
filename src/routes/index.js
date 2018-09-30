19b9a49e08852935e8cc137e8ee781b07a9bba3e
Tanner Muro <tmuro17@gmail.com>
2018-09-30 15:55:39 -0400

    Get files mostly done, in theory

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const gridfs = require('gridfs-stream');
 const router = express.Router();
-const MongoDB   
+const MongoDB;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-    // Upload a file from loca file-system to MongoDB
+    // Upload a file from local file-system to MongoDB
 router.get('/api/file/upload', (req, res) => {
 
   var filename = req.query.filename;
@@ -14,6 +15,24 @@ router.get('/api/file/upload', (req, res) => {
   fs.createReadStream("../Fractal.png").pipe(writestream);
   writestream.on('close', (file) => {
     res.send('Stored File: ' + file.filename);
+router.get('/getFiles', function (req, res, next) {
+  gridfs.files.find({filename}).toArray((err, files) => {
+    if (err) res.send(err);
+    res.json(files);
+  });
+});
+
+router.post('/putFile', function (req, res, next) {
+// Connect to the db
+  MongoClient.connect("mongodb://66.175.211.166/test", function (err, db) {
+    if (err) return console.dir(err);
+    var grid = new Grid(db, 'fs');
+    var buffer = new Buffer("Hello world");
+    grid.put(buffer, {metadata: {category: 'text'}, content_type: 'text'}, function (err, fileInfo) {
+      if (!err) {
+        console.log("Finished writing file to Mongo");
+      }
+    });
   });
 });
 
