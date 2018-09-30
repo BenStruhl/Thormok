3aa0e9cef0b0973bed80e170b2c33298fc94bff5
benstruhl <struhl.b@husky.neu.edu>
2018-09-30 14:57:24 -0400

    added MongoClient

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var MongoClient = require('mongodb').MongoClient,
+  Grid = MongoClient.Grid;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
