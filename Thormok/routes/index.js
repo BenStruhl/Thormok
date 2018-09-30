655922105ac82e688ff9bd4831fa616a6d0587ea
benstruhl <struhl.b@husky.neu.edu>
2018-09-30 14:43:48 -0400

    added gitignore

--- Thormok/routes/index.js
+++ Thormok/routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient,
+  Grid = mongo.Grid;
 
-/* GET home page. */
+
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('respond with a resource');
+});
+
+router.post('/putFile', function(req, res, next) {
+// Connect to the db
+  MongoClient.connect("mongodb://66.175.211.166/test", function(err, db) {
+    if(err) return console.dir(err);
+    var grid = new Grid(db, 'fs');
+    var buffer = new Buffer("Hello world");
+    grid.put(buffer, {metadata:{category:'text'}, content_type: 'text'}, function(err, fileInfo) {
+      if(!err) {
+        console.log("Finished writing file to Mongo");
+      }
+    });
+  });
 });
 
 module.exports = router;
